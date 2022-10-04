2ed9f1c5de8be8ad3ad3bd4229a87c3c18cce5e4
Luis Augusto Jarillo <pgd_online@hotmail.com>
2022-10-04 17:34:54 -0300

    novas rotas

--- app.js
+++ app.js
@@ -21,6 +21,39 @@ venom
 
 function start(client) {
 
+    //5511975654552-1542017101
+
+    app.post('/send-group', [
+        body('number').notEmpty(),
+        body('message').notEmpty(),
+    ], async (req, res) => {
+        const number = req.body.number;
+        const message = req.body.message;
+        try {
+            await client
+                .sendText(number + '@g.us', '😜 iLembrei: ' + message)
+                .then((result) => {
+                    console.log('Result: ', result); //return object success
+                    return res.status(200).json({
+                        status: "success",
+                        message: "enviado"
+                    })
+                })
+                .catch((erro) => {
+                    console.error('Error when sending: ', erro); //return object error
+                    return res.status(500).json({
+                        status: "error",
+                        message: erro.text
+                    })
+                });
+        } catch (e) {
+            return res.status(500).json({
+                status: "error",
+                message: "error"
+            })
+        }
+    })
+
     app.post('/send', [
         body('number').notEmpty(),
         body('message').notEmpty(),
@@ -41,9 +74,165 @@ function start(client) {
                     console.error('Error when sending: ', erro); //return object error
                     return res.status(500).json({
                         status: "error",
-                        message: "error"
+                        message: erro.text
                     })
                 });
+        } catch (e) {
+            return res.status(500).json({
+                status: "error",
+                message: "error"
+            })
+        }
+    })
+
+    app.post('/send-buttons', [
+        body('number').notEmpty(),
+        body('title').notEmpty(),
+        body('message').notEmpty(),
+    ], async (req, res) => {
+        const number = req.body.number;
+        const title = req.body.title;
+        const message = req.body.message;
+        const buttons = [
+            {
+                "buttonText": {
+                    "displayText": "Sim"
+                }
+            },
+            {
+                "buttonText": {
+                    "displayText": "Não"
+                }
+            }
+        ]
+        try {
+            await client
+                .sendButtons(number + '@c.us', title, buttons, '😜 iLembrei: ' + message)
+                .then((result) => {
+                    console.log('Result: ', result); //return object success
+                    return res.status(200).json({
+                        status: "success",
+                        message: "enviado"
+                    })
+                })
+                .catch((erro) => {
+                    console.error('Error when sending: ', erro); //return object error
+                    return res.status(500).json({
+                        status: "error",
+                        message: erro.text
+                    })
+                });
+        } catch (e) {
+            return res.status(500).json({
+                status: "error",
+                message: "error"
+            })
+        }
+    })
+
+    app.post('/send-mp3', [
+        body('number').notEmpty(),
+    ], async (req, res) => {
+        const number = req.body.number;
+        try {
+            await client.sendVoice(number + '@c.us', './mp3/ola.mp3')
+                .then((result) => {
+                    console.log('Result: ', result); //return object success
+                    return res.status(200).json({
+                        status: "success",
+                        message: "enviado"
+                    })
+                })
+                .catch((erro) => {
+                    console.error('Error when sending: ', erro); //return object error
+                    return res.status(500).json({
+                        status: "error",
+                        message: erro.text
+                    })
+                });
+        } catch (e) {
+            return res.status(500).json({
+                status: "error",
+                message: "error"
+            })
+        }
+    })
+
+    app.post('/send-img', [
+        body('number').notEmpty(),
+    ], async (req, res) => {
+        const number = req.body.number;
+        try {
+            await client
+                .sendImage(
+                    number + '@c.us',
+                    '/img/arnald.png',
+                    'Aranald',
+                    'Aranald'
+                )
+                .then((result) => {
+                    console.log('Result: ', result); //return object success
+                    return res.status(200).json({
+                        status: "success",
+                        message: "enviado"
+                    })
+                })
+                .catch((erro) => {
+                    console.error('Error when sending: ', erro); //return object error
+                    return res.status(500).json({
+                        status: "error",
+                        message: erro.text
+                    })
+                });
+        } catch (e) {
+            return res.status(500).json({
+                status: "error",
+                message: "error"
+            })
+        }
+    })
+
+    app.post('/img-to-sticker', [
+        body('number').notEmpty(),
+    ], async (req, res) => {
+        const number = req.body.number;
+        try {
+            await client
+                .sendImageAsStickerGif(
+                    number + '@c.us',
+                    './img/cat.gif'
+                )
+                .then((result) => {
+                    console.log('Result: ', result); //return object success
+                    return res.status(200).json({
+                        status: "success",
+                        message: "enviado"
+                    })
+                })
+                .catch((erro) => {
+                    console.error('Error when sending: ', erro); //return object error
+                    return res.status(500).json({
+                        status: "error",
+                        message: erro.message
+                    })
+                });
+        } catch (e) {
+            return res.status(500).json({
+                status: "error",
+                message: "error"
+            })
+        }
+    })
+
+    app.post('/get-all-chats', [], async (req, res) => {
+        try {
+            const chats = await client.getAllChats();
+            console.log(chats);
+            return res.status(200).json({
+                status: "success",
+                message: "enviado",
+                chats: chats
+            })
         } catch (e) {
             return res.status(500).json({
                 status: "error",
